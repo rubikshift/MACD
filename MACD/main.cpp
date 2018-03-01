c8624034cb6adffec1ac46a36294f0f4234b5a46
Michał Krakowiak <michal.krak97@gmail.com>
2018-03-01 17:48:05 +0100

    ema calc bugfix

--- MACD/main.cpp
+++ MACD/main.cpp
@@ -16,46 +16,47 @@ struct comma_separator : std::numpunct<char>
 	}
 };
 
-void generateWeights(long double* weights, unsigned int period)
+void generateWeights(double* weights, const unsigned int period)
 {
-	long double w, alpha = 2 / (period + 1);
-	w = 1 - alpha;
-	weights[0] = 1;
-	for (unsigned int i = 1; i < period + 1; i++)
+	double w, alpha = 2.0 / (period - 1.0);
+	w = 1;
+	for (unsigned int i = 0; i < period + 1; i++)
 	{
 		weights[i] = w;
-		w *= 1 - alpha;
+		w *= (1.0 - alpha);
 	}
-	long double sum = 0;
+	double sum = 0;
 	for (unsigned int i = 0; i < period + 1; i++)
 		sum += weights[i];
 
 	weights[period + 1] = sum; //na koncu tablicy z wagami - suma wag, dla optymalizacji obliczen
 }
 
-long double calcEMA(const long double* data, unsigned int start, unsigned int period, const long double* weights)
+double calcEMA(const double* data, const unsigned int start, const unsigned int period, const double* weights)
 {
-	long double EMA = 0;
-	if (start < period)
-		return EMA; // co w przypadku, gdy okres wyszedlby za tablice?
+	double EMA = 0;
 	for (unsigned int i = 0; i < period + 1; i++)
+	{
+		if (start < i)
+			break;
 		EMA += weights[i] * data[start - i];
+	}
 	EMA /= weights[period + 1];
 	return EMA;
 }
 
-long double calcMACD(const long double* data, unsigned int i, const long double* weights12, const long double* weights26)
+double calcMACD(const double* data, unsigned int i, const double* weights12, const double* weights26)
 {
-	long double EMA12 = calcEMA(data, i, PERIOD12, weights12);
-	long double EMA26 = calcEMA(data, i, PERIOD26, weights26);
-	return EMA12 - EMA26;
+	double EMA12 = calcEMA(data, i, PERIOD12, weights12);
+	double EMA26 = calcEMA(data, i, PERIOD26, weights26);
+	return (EMA12 - EMA26);
 }
 
 int main()
 {
-	long double data[SIZE], MACD[SIZE];
-	long double weights12[PERIOD12 + 2], weights26[PERIOD26 + 2], weights9[PERIOD9 + 2]; // tablice z wagami, dal optymalizacji obliczen
-	long double signal;
+	double data[SIZE], MACD[SIZE];
+	double weights12[PERIOD12 + 2], weights26[PERIOD26 + 2], weights9[PERIOD9 + 2]; // tablice z wagami, dla optymalizacji obliczen
+	double signal, histogram;
 
 	//init
 	generateWeights(weights12, PERIOD12);
@@ -64,6 +65,7 @@ int main()
 
 	std::ofstream macdFile("macd.txt", std::ios::out);
 	std::ofstream signalFile("signal.txt", std::ios::out);
+	std::cin.imbue(std::locale(std::cout.getloc(), new comma_separator));
 	signalFile.imbue(std::locale(std::cout.getloc(), new comma_separator));
 	macdFile.imbue(std::locale(std::cout.getloc(), new comma_separator));
 
@@ -74,6 +76,7 @@ int main()
 	{
 		MACD[i] = calcMACD(data, i, weights12, weights26);
 		signal = calcEMA(MACD, i, PERIOD9, weights9);
+		histogram = MACD[i] - signal;
 		macdFile << MACD[i] << "\n";
 		signalFile << signal << "\n";
 	}
